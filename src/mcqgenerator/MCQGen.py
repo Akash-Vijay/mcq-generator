77f92886dab0676d46e415359aa0b66e4867b1bf
Akash-Vijay <akashvijay789@gmail.com>
2024-04-06 12:15:32 +0530

    [update] added quiz evaluation chain in modular code

--- src/mcqgenerator/MCQGen.py
+++ src/mcqgenerator/MCQGen.py
@@ -38,3 +38,22 @@ quiz_prompt = PromptTemplate(template=TEMPLATE, input_variables=[
 
 quiz_chain = LLMChain(llm=llm, prompt=quiz_prompt,
                       output_key="quiz", verbose=True)
+
+
+# Defining the Prompt Template and the Chain for the Quiz Evaluation
+TEMPLATE2 = """
+You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
+You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use at max 50 words for complexity analysis. 
+if the quiz is not at per with the cognitive and analytical abilities of the students,\
+update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities
+Quiz_MCQs:
+{quiz}
+
+Check from an expert English Writer of the above quiz:
+"""
+
+evaluation_prompt = PromptTemplate(
+    template=TEMPLATE2, input_variables=["subject", "quiz"])
+
+review_chain = LLMChain(llm=llm, prompt=evaluation_prompt,
+                        output_key="review", verbose=True)
