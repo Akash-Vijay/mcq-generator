6dec4c21ea9154ff20ec713cf9b7f609e84b2d77
Akash-Vijay <akashvijay789@gmail.com>
2024-04-06 14:19:19 +0530

    [upddate] restored modular code structure

---
+++ StreamlitAPP.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+import json
+import traceback
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.logger import logging
+import streamlit as st
+from src.mcqgenerator.MCQGen import generate_evaluate_chain
+
